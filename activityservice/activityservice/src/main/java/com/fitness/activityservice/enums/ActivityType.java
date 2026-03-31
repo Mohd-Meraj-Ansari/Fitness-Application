73af1a9e8f15730f99188b49f24f8a810df28aa1
Meraj Ansari <meraj6316@gmail.com>
2026-03-31 10:45:52 +0530

    feat: add fitness activity service and integrate Eureka server

---
+++ activityservice/activityservice/src/main/java/com/fitness/activityservice/enums/ActivityType.java
@@ -0,0 +1,5 @@
+package com.fitness.activityservice.enums;
+
+public enum ActivityType {
+    RUNNING, WALKING, CYCLING, SWIMMING, WEIGHT_TRAINING, YOGA, CARDIO, STRETCHING
+}
---
+++ activityservice/activityservice/src/main/java/com/fitness/activityservice/service/ActivityService.java
@@ -0,0 +1,60 @@
+package com.fitness.activityservice.service;
+
+import com.fitness.activityservice.dto.ActivityRequest;
+import com.fitness.activityservice.dto.ActivityResponse;
+import com.fitness.activityservice.entity.Activity;
+import com.fitness.activityservice.repository.ActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ActivityService {
+
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    public ActivityResponse trackActivity(ActivityRequest request) {
+        Activity activity = Activity.builder()
+                .userId(request.getUserId())
+                .type(request.getType())
+                .duration(request.getDuration())
+                .caloriesBurned(request.getCaloriesBurned())
+                .startTime(request.getStartTime())
+                .additionalMetrics(request.getAdditionalMetrics())
+                .build();
+        Activity saveActivity = activityRepository.save(activity);
+        return mapToResponse(saveActivity);
+    }
+
+    private ActivityResponse mapToResponse(Activity activity) {
+        ActivityResponse activityResponse = new ActivityResponse();
+        activityResponse.setId(activity.getId());
+        activityResponse.setUserId(activity.getUserId());
+        activityResponse.setType(activity.getType());
+        activityResponse.setDuration(activity.getDuration());
+        activityResponse.setCaloriesBurned(activity.getCaloriesBurned());
+        activityResponse.setStartTime(activity.getStartTime());
+        activityResponse.setAdditionalMetrics(activity.getAdditionalMetrics());
+        activityResponse.setCreatedAt(activity.getCreatedAt());
+        activityResponse.setUpdatedAt(activity.getUpdatedAt());
+
+        return activityResponse;
+    }
+
+    public List<ActivityResponse> getUserActivities(String userId) {
+        List<Activity> activities = activityRepository.findByUserId(userId);
+
+        return activities.stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+    }
+
+    public ActivityResponse getActivityById(String activityId) {
+        return activityRepository.findById(activityId)
+                .map(this::mapToResponse)
+                .orElseThrow(() -> new RuntimeException("activity not found with id " + activityId));
+    }
+}
